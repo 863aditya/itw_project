7921addea7eba74e1e121c3521ce1bbcdbf04d69
Aditya <863aditya@gmail.com>
2022-11-14 23:41:52 +0530

    forum_testing_completed

--- forum/views.py
+++ forum/views.py
@@ -5,17 +5,17 @@ from django.contrib.auth import logout, authenticate, login
 from forum.models import fmsg
  
 # Create your views here.
-
+LIMIT=5
 def pg(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         if request.method=='POST':
             a1=request.user.username
-            a2=request.POST.get('content')
+            a2=request.POST.get('description')
             a2=fmsg(posted_by=a1,content=a2)
             a2.save()
         q1=fmsg.objects.all()
         d1=dict()
         for x in range(len(q1)):
-            d1[x+1]=[q1[x].posted_by,q1[x].content]
+                d1[x+1]=[q1[x].posted_by,q1[x].content]
         return render(request,'forum.html',{'d1':d1})
     return redirect('/p/')
